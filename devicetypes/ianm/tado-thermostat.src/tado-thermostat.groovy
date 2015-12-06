da903f93284bd812737b0556e3f3db02b4b364de
Ian M <dev@moveandfire.co.uk>
2015-12-06 18:10:33 +0000

    Initial working version of Tado SmartThings Device Type, read-only.

---
+++ devicetypes/ianm/tado-thermostat.src/tado-thermostat.groovy
@@ -0,0 +1,161 @@
+/**
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *	Tado Thermostat
+ *
+ *	Author: Ian M
+ *	Date: 2015-12-04
+ */
+preferences {
+	input("username", "text", title: "Username", description: "Your Tado username")
+	input("password", "password", title: "Password", description: "Your Tado password")
+}  
+ 
+metadata {
+	definition (name: "Tado Thermostat", namespace: "ianm", author: "Ian M") {
+		capability "Actuator"
+        capability "Temperature Measurement"
+		capability "Thermostat"
+        //capability "Presence Sensor"
+		capability "Polling"
+		capability "Refresh"
+	}
+
+	// simulator metadata
+	simulator {
+		// status messages
+
+		// reply messages
+	}
+
+	tiles(scale: 2){
+      	multiAttributeTile(name: "thermostat", width: 6, height: 4, type:"lighting") {
+			tileAttribute("device.temperature", key:"PRIMARY_CONTROL", canChangeIcon: true, canChangeBackground: true){
+            	//attributeState "HOME", label:'${currentValue}° C', unit:"C", backgroundColor:"#fab907", icon:"st.Home.home2"
+            	//attributeState "AWAY", label:'${currentValue}° C', unit:"C", backgroundColor:"#62aa12", icon:"st.Outdoor.outdoor18"
+            	//attributeState "SLEEP", label:'${currentValue}° C', unit:"C", backgroundColor:"#0164a8", icon:"st.Bedroom.bedroom2"
+            	attributeState "default", label:'${currentValue}° C', unit:"C", backgroundColor:"#fab907", icon:"st.Home.home1"
+            }
+            tileAttribute ("controlPhase", key: "SECONDARY_CONTROL") {
+				attributeState "controlPhase", label:'${currentValue}'
+			}
+		}
+        
+        valueTile("setPointTemp", "device.setPointTemp", width: 2, height: 2, decoration: "flat") {
+			state("default", label: '${currentValue}° C')
+		}
+
+        standardTile("autoOperation", "device.autoOperation", width: 2, height: 2, canChangeBackground:true) {
+            state "HOME", label:'${name}', backgroundColor:"#fab907", icon:"st.Home.home2"
+            state "AWAY", label:'${name}', backgroundColor:"#62aa12", icon:"st.Outdoor.outdoor18"
+            state "SLEEP", label:'${name}', backgroundColor:"#0164a8", icon:"st.Bedroom.bedroom2"
+            state "OFF", label:'${name}', backgroundColor:"#ffffff", icon:"st.switches.switch.off"
+            state "MANUAL", label:'${name}', backgroundColor:"#ffffff", icon:"st.Weather.weather1"
+		}
+        
+        /*standardTile("presence", "device.presence", width: 2, height: 2, canChangeBackground: true) {
+			state "present", labelIcon:"st.presence.tile.present", backgroundColor:"#53a7c0"
+			state "not present", labelIcon:"st.presence.tile.not-present", backgroundColor:"#ebeef2"
+		}*/
+      	
+        standardTile("refresh", "device.switch", inactiveLabel: false, width: 2, height: 2, decoration: "flat") {
+			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+		
+		main "thermostat"
+		details (["thermostat","refresh","setPointTemp","autoOperation"])
+	}
+}
+
+// Parse incoming device messages to generate events
+private parseResponse(resp) {
+    log.debug("Executing parseResponse: "+resp.data)
+    //log.debug("Output success: "+resp.data.success)
+    log.debug("Output status: "+resp.status)
+    if(resp.status == 200) {
+    	log.debug("Executing parseResponse.successTrue")
+        
+        def temperature = Math.round(resp.data.insideTemp)
+        log.debug("Read temperature: " + temperature)
+        
+        def controlPhase = resp.data.controlPhase
+        if(resp.data.controlPhase == "UNDEFINED"){
+        	controlPhase = "FROST PROTECTION"
+        }
+        log.debug("Read controlPhase: " + controlPhase)
+        
+        def setPointTemp = Math.round(resp.data.setPointTemp)
+        log.debug("Read setPointTemp: " + setPointTemp)
+        
+        def autoOperation = resp.data.autoOperation
+        if(resp.data.operation == "NO_FREEZE"){
+        	autoOperation = "OFF"
+        }else if(resp.data.operation == "MANUAL"){
+        	autoOperation = "MANUAL"
+        }
+        log.debug("Read autoOperation: " + autoOperation)
+        
+        def temperatureUnit = "C"
+        
+        sendEvent(name: 'temperature', value: temperature, unit: temperatureUnit)
+        sendEvent(name: 'setPointTemp', value: setPointTemp, unit: temperatureUnit)
+		sendEvent(name: 'controlPhase', value: controlPhase)
+        sendEvent(name: 'autoOperation', value: autoOperation)
+        
+    }else if(resp.status == 201){
+        log.debug("Something was created/updated")
+    }
+}
+
+
+def poll() {
+	log.debug "Executing 'poll'"
+	refresh()
+}
+
+def refresh() {
+	log.debug "Executing 'refresh'"
+    sendCommand("getCurrentState")
+}
+
+private sendCommand(path, method="GET", body=null) {
+    //def accessToken = getAccessToken()
+    def pollParams = [
+        uri: "https://my.tado.com",
+        path: "/mobile/1.9/getCurrentState",
+        requestContentType: "application/json",
+    	query: [username:settings.username, password:settings.password],
+        body: body
+    ]
+    log.debug(method+" Http Params ("+pollParams+")")
+    
+    try{
+        if(method=="GET"){
+        	log.debug "Executing 'sendCommand.GET'"
+            httpGet(pollParams) { resp ->            
+                //log.debug resp.data
+                parseResponse(resp)
+            }
+        }else if(method=="PUT") {
+        	log.debug "Executing 'sendCommand.PUT'"
+            httpPut(pollParams) { resp ->            
+                parseResponse(resp)
+            }
+        }
+    } catch(Exception e){
+        debug("___exception: " + e)
+    }
+}
+
+
+
+// Commands to device
